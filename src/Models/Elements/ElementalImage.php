4c5ab26a205a02bf2bbb300422aff66ec1b31a6f
Mark Taylor <tardinha@gmail.com>
2021-01-18 09:45:14 +1100

    Initial commit

---
+++ src/Models/Elements/ElementalImage.php
@@ -0,0 +1,95 @@
+<?php
+namespace NSWDPC\Elemental\Models\Image;
+
+use DNADesign\Elemental\Models\BaseElement;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\DropdownField;
+
+/**
+ * ElementImage adds an image with some config
+ */
+class ElementImage extends BaseElement
+{
+    private static $icon = "font-icon-block-banner";
+
+    private static $table_name = "ElementImage";
+
+    private static $title = "Image";
+    private static $description = "Display an image";
+
+    private static $singular_name = "Image";
+    private static $plural_name = "Images";
+
+    public function getType()
+    {
+        return _t(__CLASS__ . ".BlockType", "Image");
+    }
+
+    private static $db = [
+        "Width" => "Varchar",
+        "Height" => "Varchar",
+    ];
+
+    private static $has_one = [
+        "Image" => Image::class,
+    ];
+
+    private static $summary_fields = [
+        "Image.CMSThumbnail" => "Image",
+        "Title" => "Title",
+    ];
+
+    private static $owns = ["Image"];
+
+    public function getAllowedFileTypes()
+    {
+        $types = $this->config()->get("allowed_file_types");
+        if (empty($types)) {
+            $types = ["jpg", "jpeg", "gif", "png", "webp"];
+        }
+        $types = array_unique($types);
+        return $types;
+    }
+
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function ($fields) {
+            $fields->addFieldsToTab("Root.Main", [
+                DropdownField::create(
+                    "Width",
+                    _t(__CLASS__ . ".WIDTH", "Width"),
+                    [
+                        "container" => "Content width",
+                        "full" => "Browser width",
+                    ]
+                ),
+                DropdownField::create(
+                    "Height",
+                    _t(__CLASS__ . ".HEIGHT", "Height"),
+                    [
+                        "small" => "Small",
+                        "medium" => "Medium",
+                        "large" => "Large",
+                    ]
+                ),
+                UploadField::create(
+                    "Image",
+                    _t(__CLASS__ . ".SLIDE_IMAGE", "Image")
+                )
+                    ->setFolderName("videos/" . $this->ID)
+                    ->setAllowedExtensions($this->getAllowedFileTypes())
+                    ->setDescription(
+                        sprintf(
+                            _t(
+                                __CLASS__ . "ALLOWED_FILE_TYPES",
+                                "Allowed file types: %s"
+                            ),
+                            implode(",", $this->getAllowedFileTypes())
+                        )
+                    ),
+            ]);
+        });
+        return parent::getCMSFields();
+    }
+}
---
+++ templates/NSWDPC/Elemental/Models/Image/ElementImage.ss
@@ -0,0 +1,1 @@
+<img src="{$Image.URL}" alt="<% if $Title %>{$Title.XML}<% end_if %>">
